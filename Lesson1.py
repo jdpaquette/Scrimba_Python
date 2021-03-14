e8c4f0629603fd5fe70fc3578a00014c6760e593
Joseph Paquette <josephdpaquette@gmail.com>
2021-03-14 15:42:56 -0400

    lesson 1

---
+++ Lesson1.py
@@ -0,0 +1,29 @@
+# String Exercise
+# name = input('What is your name?: ')
+# kilometers = float(input('What is the distance in km?: '))
+# miles = round(kilometers / 1.609, 2)
+# print(f"Hello {name.capitalize()}! Your distance is {kilometers} km or {miles} miles!")
+
+
+# Lists Exercise
+# sales_w1 = [7,3,42,19,15,35,9]
+# sales_w2 = [12,4,26,10,7,28]
+# sales_w2.append(int(input("What is today's lemonade sales?: ")))
+# sales = sales_w1 + sales_w2
+#
+# print(f"Best sales day was ${max(sales)*1.5}")
+# print(f"Worst sales day was ${min(sales)*1.5}")
+# print(f"Total sales were ${sum(sales)*1.5}")
+
+
+# Split and Join Exercise.
+# csv = 'Eric,John,Michael,Terry,Graham:TerryG;Brian'
+# friends_list = ['Exercise: fill me with names']
+# friends_list = (",".join(",".join(csv.split(";")).split(":"))).split(",")
+# print(friends_list)
+# From the list above fill a list(friends_list) properly
+# with the names of all the friends. One per "slot"
+# you may need to run same command several times
+# use print() statements to work your way through the exercise
+
+
